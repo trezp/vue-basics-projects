7d96b23374b9e0f44f8ed0dcb238166d4580b736
Treasure <treasure.rose@gmail.com>
2018-02-27 21:21:58 -0800

    working sorta

--- flashcard_components/app.js
+++ flashcard_components/app.js
@@ -16,52 +16,19 @@ const cards = [
   },
 ]
 
-// THIS IS BROKEN WAAAAH
-Vue.component('flashcard-form', {
-  props: ['cardFront', 'cardBack'],
-  template:
-  `<div>
-    <label for="front">Front
-      <input v-model="cardFront" type="text" id="front">
-    </label>
-    <label for="back">Back
-      <input v-model="cardBack" type="text" id="back">
-    </label>
-    <button v-on:click="addNew">Add a New Card</button>
-  </div>
-  `
-})
+Vue.component('flash-card', {
+  template: '#flashcard-template',
+  props: ['cards', 'message'],
+});
 
-new Vue({
-  el: '#flashCardList',
-  template: `
-    <div>
-    <flashcard-form v-bind:cardFront="cardFront" v-bind:cardBack="cardBack"></flashcard-form>
-    <ul>
-      <li v-for="(card, index) in cards" class="card" v-on:click="toggleCard(card)">
-        <span v-on:click="cards.splice(index, 1)">X</span>
-        <p v-show="!card.active" class="front">{{card.front}}</p>
-        <p v-show="card.active" class="back">{{card.back}}</p>
-      </li>
-    </ul>
-    </div>`,
-  data: {
-    cards: cards,
-    cardFront: '',
-    cardBack: '',
-  },
-  methods: {
-    toggleCard: function(card){
-      card.active ? card.active = false : card.active = true;
-    },
-    addNew: function(){
-      this.cards.push({
-        'front': this.cardFront,
-        'back': this.cardBack,
-        'active': false
-      });
-      this.cardFront = '';
-      this.cardBack = '';
-    },
+const app = new Vue({
+  el: '#app',
+  data: function() {
+    return {
+      cards: cards,
+      cardFront: '',
+      cardBack: '',
+      message: 'HELLO!!!!!'
+    }
   }
 });
