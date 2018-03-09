ca7532bd64897c1978803014f39ee3862a1dc644
Treasure <treasure.rose@gmail.com>
2018-03-09 15:00:16 -0800

    before refactor

--- flashcard_components/app.js
+++ flashcard_components/app.js
@@ -1,4 +1,4 @@
-const cards = [
+const deck = [
   {
     'front': 'This is the front side of the card.',
     'back': 'This is the back side of the card.',
@@ -16,19 +16,47 @@ const cards = [
   },
 ]
 
+const deck2 = [
+  {
+    'front': 'What is the shortcut to bind a class to an element?',
+    'back': 'Answer',
+    'active': false
+  },
+  {
+    'front': 'This is card 2',
+    'back': 'This is the back side of card 2.',
+    'active': false
+  },
+  {
+    'front': 'This is card 3',
+    'back': 'This is the back side of card 3.',
+    'active': false
+  },
+]
 Vue.component('flash-card', {
   template: '#flashcard-template',
-  props: ['cards', 'message'],
+  props: ['decks'],
+  methods: {
+    pleaseDelete: function(card){
+      console
+    }
+  }
 });
 
 const app = new Vue({
   el: '#app',
   data: function() {
     return {
-      cards: cards,
+      decks: [deck, deck2],
       cardFront: '',
       cardBack: '',
-      message: 'HELLO!!!!!'
+    }
+  },
+  methods: {
+    handleDelete: function(card){
+      console.log(card.front);
     }
   }
+
 });
+
