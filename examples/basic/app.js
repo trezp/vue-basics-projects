9a82617bab60a483c2d85fd3262f8b443ea8e958
Treasure <treasure.rose@gmail.com>
2018-03-13 22:33:02 -0700

    adjusted second code example

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -1,29 +1,29 @@
 
 /////// HELLO VUE EXAMPLE ///////
-const helloVue = new Vue({
-  el: '#hello-vue',
-  data: {
-    title: 'Hello World!',
-    message: 'This is a message!'
-  },
-  methods: {
-    sayHello1: function(){
-      alert('Hello');
-    },
-    sayHello2: function(){
-      alert(this.message);
-    }
-  }
-});
+// const helloVue = new Vue({
+//   el: '#hello-vue',
+//   data: {
+//     title: 'Hello World!',
+//     message: 'This is a message!'
+//   },
+//   methods: {
+//     sayHello1: function(){
+//       alert('Hello');
+//     },
+//     sayTitle: function(){
+//       alert(this.title);
+//     }
+//   }
+// });
 
-const helloVue2 = new Vue({
-  el: '#hello-vue2',
-  data: {
-    title: 'Also hello, world!',
-    message: 'This is also a message!',
-    message2: 'Adding another binding is easy.'
-  }
-});
+// const helloVue2 = new Vue({
+//   el: '#hello-vue2',
+//   data: {
+//     title: 'Also hello, world!',
+//     message: 'This is also a message!',
+//     message2: 'Adding another binding is easy.'
+//   }
+// });
 
 ///////////// SINGLE MEDIA ITEM ///////////
 // Do this example, only with showDetail as well
@@ -33,11 +33,19 @@ const bookDetail = new Vue({
     title: 'The Sirens of Titan',
     author: 'Kurt Vonnegut',
     summary: "This is a summary of the Sirens of Titan.",
-    type: 'book'
+    type: 'book',
+    showDetail: false
+  },
+  methods: {
+    toggleDetails: function(){
+      this.showDetail ? this.showDetail = false : this.showDetail = true;
+    }
   }
 });
 
 ///////////////// DISPLAYING A LIST/ SIMPLE ACCORDIAN ////////////
+//Instead of an accordian, make a select menu that we can use to filter the list
+//Probably also make a search
 const mediaList = new Vue({
   el: '#media-list',
   data: {
