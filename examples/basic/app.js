49822041f3aec282dc0d626026e0957a9cf3ce00
Treasure <treasure.rose@gmail.com>
2018-04-30 21:49:35 -0700

    minor adjustments

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -21,6 +21,9 @@ const example = new Vue({
   methods: {
     sayHello: function(){
       alert(this.title);
+    },
+    enableButton: function(){
+      this.isDisabled = false; 
     }
   }
 });
@@ -32,7 +35,7 @@ const bookDetail = new Vue({
   data: {
     title: 'The Sirens of Titan',
     author: 'Kurt Vonnegut',
-    summary: "This is a summary of the Sirens of Titan.",
+    summary: 'This is a summary of the Sirens of Titan.',
     type: 'book',
     showDetail: false
   },
@@ -54,63 +57,65 @@ const arrExample = new Vue({
   }
 });
 ///////////////// DISPLAYING AND SORTING A LIST ////////////
+const mediaList = 
+  [
+    {
+      title: 'Hop on Pop', 
+      description: "A delightful children's book.",
+      tag: 'book',
+      contributor: 'Dr. Suess',
+      showDetail: false,
+    },
+    {
+      title: 'The Joy of Painting', 
+      description: "Create a world of happy little trees!",
+      tag: 'video',
+      contributor: 'Bob Ross',
+      showDetail: false
+    },
+    {
+      title: 'Supernatural: The Complete 12th Season', 
+      description: "A (literally) neverending roadtrip.",
+      tag: 'video',
+      contributor: "Eric Kripke",
+      showDetail: false
+    },
+    {
+      title: 'Planet Earth II', 
+      description: "Hours of beautiful but heart attack-inducing nature footage",
+      tag: 'streaming video',
+      contributor: 'David Attenborough',
+      showDetail: false,
+    },
+    {
+      title: 'Titanic', 
+      description: "The boat sinks.",
+      tag: 'video',
+      contributor: 'James Cameron',
+      showDetail: false,
+    },
+    {
+      title: 'The Sirens of Titan', 
+      description: "Mankind flung its advance agents ever outward, ever outward.",
+      tag: 'book',
+      contributor: 'Kurt Vonnegut',
+      showDetail: false,
+    },
+    {
+      title: 'Better Call Saul', 
+      description: "A slow-burning Breaking Bad prequel.",
+      tag: 'streaming video',
+      contributor: 'someone',
+      showDetail: false,
+    }
+  ]
 
 const app = new Vue({
   el: '#media-list',
   data: {
     title: 'Treehouse Public Library',
     type: '',
-    mediaList: [
-      {
-        title: 'Hop on Pop', 
-        description: "A delightful children's book.",
-        tag: 'book',
-        contributor: 'Dr. Suess',
-        showDetail: false,
-      },
-      {
-        title: 'The Joy of Painting', 
-        description: "Create a world of happy little trees!",
-        tag: 'video',
-        contributor: 'Bob Ross',
-        showDetail: false
-      },
-      {
-        title: 'Supernatural: The Complete 12th Season', 
-        description: "A (literally) neverending roadtrip.",
-        tag: 'video',
-        contributor: "Eric Kripke",
-        showDetail: false
-      },
-      {
-        title: 'Planet Earth II', 
-        description: "Hours of beautiful but heart attack-inducing nature footage",
-        tag: 'streaming video',
-        contributor: 'David Attenborough',
-        showDetail: false,
-      },
-      {
-        title: 'Titanic', 
-        description: "The boat sinks.",
-        tag: 'video',
-        contributor: 'James Cameron',
-        showDetail: false,
-      },
-      {
-        title: 'The Sirens of Titan', 
-        description: "Mankind flung its advance agents ever outward, ever outward.",
-        tag: 'book',
-        contributor: 'Kurt Vonnegut',
-        showDetail: false,
-      },
-      {
-        title: 'Better Call Saul', 
-        description: "A slow-burning Breaking Bad prequel.",
-        tag: 'streaming video',
-        contributor: 'someone',
-        showDetail: false,
-      }
-    ]
+    mediaList: mediaList
   },
   methods: {
     toggleDetails: function(media){
@@ -132,3 +137,4 @@ const app = new Vue({
     }
   }
 });
+
